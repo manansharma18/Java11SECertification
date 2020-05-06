414d43df8cb6def3248d898a3d687bd325f27b6b
manansharma18 <manansharma18@gmail.com>
2020-05-06 11:57:21 -0400

    HackerRankStrangeCounter

---
+++ com.oracle.demos/src/HackerRankRandomStrangeCounter.java
@@ -0,0 +1,56 @@
+public class HackerRankRandomStrangeCounter {
+
+
+
+        // Complete the strangeCounter function below.
+        public long strangeCounter(long t) {
+
+            long low = 1;
+            long high = 3;
+            boolean isValueFound = false;
+
+            long newLow;
+            long newHigh;
+
+            newLow =  low;
+            newHigh = high;
+
+
+            while(!isValueFound){
+
+                if(t>=low & t<= high){
+
+                    isValueFound = true;
+                }
+                if (!isValueFound){
+
+                    newLow = 2 * low + 2;
+                    newHigh = 2* high + 3;
+                    low = newLow;
+                    high = newHigh;
+
+                }
+            }
+
+            return  newLow + 2 - (t - newLow);
+
+
+        }
+
+        public static void main(String[] args)  {
+           var obj = new HackerRankRandomStrangeCounter();
+           long value = obj.strangeCounter(5);
+           long value1 = obj.strangeCounter(1);
+           long value2 = obj.strangeCounter(3);
+            long value3 = obj.strangeCounter(7);
+
+            if(value == 5) System.out.println("Pass");
+            if(value1 == 3) System.out.println("Pass");
+            if(value2 == 1) System.out.println("Pass");
+            if(value3 == 3) System.out.println("Pass");
+        }
+
+
+
+
+}
