e3b609b7876a9783a18cfa3a2fefeb57a2dcac8b
manansharma18 <manansharma18@gmail.com>
2020-05-07 09:55:08 -0400

    BFS tree

---
+++ com.oracle.demos/src/OperatorClass.java
@@ -0,0 +1,14 @@
+public class OperatorClass {
+
+    public static void main(String[] args) {
+        System.out.println(null == null);
+        var obj1 = new OperatorClass();
+        var obj2 = new OperatorClass();
+        OperatorClass operatorClass;
+        OperatorClass operatorClass1;
+        System.out.println(obj1 == obj2);
+        int x=1,z=4;
+        var y = x+=z;
+        
+    }
+}
