90dc775a0f08d39bb15132e31eea0fb090a08b01
manansharma18 <manansharma18@gmail.com>
2020-05-08 16:17:27 -0400

    Switch

---
+++ com.oracle.demos/src/SwitchStatements.java
@@ -0,0 +1,28 @@
+public class SwitchStatements {
+
+    public static void main(String[] args) {
+        short s = 4;
+        final short l = 5;
+        // Does not matter what the range of short is. If you add two short numbers. the result is int.
+       //final short ll =(short)(l+s);
+        final short ll =(int)6;
+        System.out.println(Short.class.isInstance(ll));
+        final int lll =(short)6;
+        System.out.println(Integer.class.isInstance(lll));
+        switch(s){
+            case l :
+                System.out.println("l");
+                break;
+
+            case ll :
+                System.out.println("ll");
+                break;
+            case 4 :
+                System.out.println("s");
+                break;
+            default:
+                System.out.println("default");
+        }
+
+    }
+}
