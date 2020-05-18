be0482e13f13c440327acd0bbf64feec8f6caa7f
manansharma18 <manansharma18@gmail.com>
2020-05-17 21:22:56 -0400

    Inheritance

---
+++ com.oracle.demos/src/Cat.java
@@ -0,0 +1,21 @@
+public class Cat extends Animal {
+    public static void testClassMethod() {
+        System.out.println("The static method in Cat");
+    }
+    public void testInstanceMethod() {
+        System.out.println("The instance method in Cat");
+    }
+
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+        Animal myAnimal = myCat;
+        Animal.testClassMethod();
+        myAnimal.testInstanceMethod();
+        testClassMethod();
+        Animal animal = new Animal();
+        animal.testInstanceMethod();
+        //Cat newCat = (Cat) animal;
+        // ClassCastException
+
+    }
+}
