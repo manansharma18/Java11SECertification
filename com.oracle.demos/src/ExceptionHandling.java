497488f53a8aba8895fe90aa1fc7238e1f23dbbb
manansharma18 <manansharma18@gmail.com>
2020-05-23 19:30:39 -0400

    Exceptions

---
+++ com.oracle.demos/src/ExceptionHandling.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        try {
+            m1(9);
+            throw new Exception("This is exception");
+            //m2();
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+     static void m1 (int x) throws CheckedExceptionHandling,IOException{
+
+        if (x>8) throw new IOException("IOException ");
+        throw new CheckedExceptionHandling("checked exception");
+
+    }
+    static void m2 () {
+        throw new UncheckedExceptionHandling("Unchecked Excpetions");
+    }
+
+    static {
+        try {
+            throw new IOException();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+class UncheckedExceptionHandling extends RuntimeException{
+
+    public UncheckedExceptionHandling(String message) {
+        super(message);
+    }
+}
+
+class CheckedExceptionHandling extends Exception{
+    public CheckedExceptionHandling(String message) {
+        super(message);
+    }
+}
