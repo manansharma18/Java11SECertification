bd59228e9b72e643b2dd6458818c4ef05cfe3177
manansharma18 <manansharma18@gmail.com>
2020-05-29 17:35:41 -0400

    Varag example

---
+++ com.oracle.demos/src/VaragWrapper.java
@@ -0,0 +1,16 @@
+public class VaragWrapper {
+
+    private static void v1(Integer x){
+        System.out.println("Integer varag");
+    }
+
+    private static void v1(long x){
+        System.out.println("long varag");
+    }
+
+
+
+    public static void main(String[] args) {
+        v1(5);
+    }
+}
