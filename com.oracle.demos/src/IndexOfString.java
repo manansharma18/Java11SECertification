8ef26edc687897ca52cf7f0ddf807622f02d8adc
manansharma18 <manansharma18@gmail.com>
2020-05-28 22:25:55 -0400

    Strings

---
+++ com.oracle.demos/src/IndexOfString.java
@@ -0,0 +1,8 @@
+public class IndexOfString {
+    public static void main(String[] args) {
+        String a = "animal";
+        System.out.println(a.indexOf("al",5));
+        System.out.println(a.substring(3,6));
+    }
+
+}
