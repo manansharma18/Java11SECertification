3f63c608412cc60ce16944657f409edc4911e4d6
manansharma18 <manansharma18@gmail.com>
2020-06-08 16:11:14 -0400

    works fine

--- com.oracle.demos/src/SwitchStatements.java
+++ com.oracle.demos/src/SwitchStatements.java
@@ -1,6 +1,22 @@
 public class SwitchStatements {
 
+
+
     public static void main(String[] args) {
+
+        OUTER: for(int i=0;i<7;i++)
+            INNER:for(int j=0;j<7;j++)
+                System.out.println(j);
+
+        int aa =9;
+        int bb =6;
+
+        if(aa>5)
+            if(bb>5)
+                System.out.println(bb);
+            else System.out.println("lol");
+         else System.out.println("hey");
+
         short s = 4;
         final short l = 5;
         // Does not matter what the range of short is. If you add two short numbers. the result is int.
@@ -19,10 +35,11 @@ public class SwitchStatements {
                 break;
             case 4 :
                 System.out.println("s");
-                break;
+                break;//return;
             default:
                 System.out.println("default");
         }
+        System.out.println("hello");
 
     }
 
