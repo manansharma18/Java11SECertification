f9c3ab634ff5dab0d076b21751123e02b44f49dd
manansharma18 <manansharma18@gmail.com>
2020-06-10 15:16:04 -0400

    works fine

---
+++ com.oracle.demos/src/Mammal.java
@@ -0,0 +1,11 @@
+public class Mammal extends Animals {
+public Integer m (String s){return 2;}
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Animals{
+    public Number m(CharSequence cs){return 1;}
+}
