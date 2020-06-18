a8e2d352b164fcf8fbba0742804d6ed58284154a
manansharma18 <manansharma18@gmail.com>
2020-06-18 09:41:48 -0400

    works fine

--- com.oracle.demos/src/Varags.java
+++ com.oracle.demos/src/Varags.java
@@ -14,11 +14,15 @@ public class Varags {
         System.out.println("long Primitive type");
     }
 
+    public static void var(int[] a, int... v){
+        System.out.println("dummies");
+    }
 
     public static void main(String[] args) {
         m1(5);
         m1(5L);
         m1();
+        var(new int[]{1,2},3);
 
     }
 
