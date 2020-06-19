de76ea1458848a2f5cf3ccdb6d697099c5912389
manansharma18 <manansharma18@gmail.com>
2020-06-19 00:21:49 -0400

    first exam review

--- com.oracle.demos/src/Mammal.java
+++ com.oracle.demos/src/Mammal.java
@@ -1,12 +1,29 @@
-public class Mammal extends Animals {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mammal extends Animals implements HasTail{
 public Integer m (String s){return 2;}
+public void hasTail(){}
 
     public static void main(String[] args) {
-        new Mammal().m("");
+
+        System.out.println(new Mammal().m(""));
+        Animals animal = new Mammal();
+        HasTail hasTail = new Mammal();
+
+        List<Mammal> mammals = new ArrayList<>();
+        mammals.add((Mammal) animal);
+        mammals.add((Mammal) hasTail);
+
+
     }
 }
 
 class Animals{
     public Number m(CharSequence cs){return 1;}
+}
+
+interface HasTail{
+    void hasTail();
 }
 
---
+++ com.oracle.demos/src/first/exam/Arthmetic.java
@@ -0,0 +1,15 @@
+package first.exam;
+
+public class Arthmetic {
+    public static void main(String[] args) {
+        System.out.println(value());
+    }
+
+    private static int value(){
+        var a = 5;
+        var b = 2;
+
+        return a++ +b;
+
+    }
+}
