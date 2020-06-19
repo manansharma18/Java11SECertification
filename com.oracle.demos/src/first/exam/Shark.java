add67f74db431fbec23cc68f2417374fe6127db3
manansharma18 <manansharma18@gmail.com>
2020-06-18 22:17:27 -0400

    first exam review

---
+++ com.oracle.demos/src/first/exam/Shark.java
@@ -0,0 +1,38 @@
+package first.exam;
+// Throws numm pointer exception
+
+public class Shark {
+
+    static Water water;
+    static Liquid liquid;
+    static final String name;
+    static {
+
+        name = "Manan";
+    }
+    public static void main(String[] args) {
+        //Liquid liquid = new Liquid();
+        System.out.println(liquid.getSomething());
+        new Water().toString();
+        water.toString(); // null pointer exception
+        water.getString();
+        new Water().getString();
+        //name = "hi";
+        System.out.println(!!true);
+    }
+
+
+
+}
+
+
+class Water{
+    public String toString(){return "";}
+    public static int getString(){return 6;}
+}
+
+class Liquid{
+    public String getSomething(){
+        return "something";
+    }
+}
