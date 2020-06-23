114cc0c5f4456b74bafbfc3ce6b593260ac591b6
manansharma18 <manansharma18@gmail.com>
2020-06-23 16:09:52 -0400

    first exam review

---
+++ com.oracle.demos/src/first/exam/ArrayAsList.java
@@ -0,0 +1,21 @@
+package first.exam;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayAsList {
+
+    public static void main(String[] args) {
+        int [] intArray = {4,5,6,7,8};
+        String [] stringArray = {"","hi","hello","hawk"};
+        ArrayList<Integer> integers = new ArrayList<>();
+        List listInteger = Arrays.asList(intArray);
+        List<String> listString = Arrays.asList(stringArray);
+        ArrayList<String> stringArrayList = new ArrayList<>(listString);
+
+        System.out.println(stringArrayList.add("sss"));
+        //System.out.println(listString.add("asas"));
+
+    }
+}
