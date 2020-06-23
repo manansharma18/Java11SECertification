c832d38907d71b93131d62f4e5b17f127db4d87d
manansharma18 <manansharma18@gmail.com>
2020-06-23 12:17:00 -0400

    first exam review

---
+++ com.oracle.demos/src/first/exam/StaticVariables.java
@@ -0,0 +1,11 @@
+package first.exam;
+
+public class StaticVariables {
+    static int x;
+    static void x(){}
+}
+
+class SubStaticVariables extends StaticVariables{
+    int x;
+    static void x(){}
+}
