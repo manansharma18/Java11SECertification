49f534f7165f0b6576d60f8df0a27196d0f64128
manansharma18 <manansharma18@gmail.com>
2020-06-23 12:28:03 -0400

    first exam review

--- com.oracle.demos/src/first/exam/StaticVariables.java
+++ com.oracle.demos/src/first/exam/StaticVariables.java
@@ -8,4 +8,23 @@ public class StaticVariables {
 class SubStaticVariables extends StaticVariables{
     int x;
     static void x(){}
+
+    void anyType(Object obj){
+        System.out.println(obj);
+    }
+
+    void checkLong(Long l){
+        System.out.println(l instanceof Long);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StaticVariables.x);
+        new SubStaticVariables().anyType(1);
+        new SubStaticVariables().anyType(1L);
+        new SubStaticVariables().anyType(1F);
+        new SubStaticVariables().anyType("1");
+        new SubStaticVariables().checkLong(1L);
+        //new SubStaticVariables().checkLong(new Integer(1));
+    }
 }
